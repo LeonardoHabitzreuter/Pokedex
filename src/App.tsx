7902d346c1b732c64096db98817df81e54de63c3
Leonardo Habitzreuter <leo.habitzreuter@gmail.com>
2024-08-16 18:48:52 -0300

    Adds a title and a bg image

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import classes from './App.module.scss'
+import pokeball from '@/assets/pokeball.svg';
+import Text from '@mui/material/Typography';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+export default function App () {
+  return (    
+    <main className={classes.container}>
+      <img src={pokeball} alt="pokeball" />
+      <Text variant='h2' fontWeight={700}>Pokedex</Text>
+    </main>
   )
 }
-
-export default App
